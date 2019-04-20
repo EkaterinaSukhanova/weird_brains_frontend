f1f981464e36a9ae32522208a56d657fe100de27
Ekaterina Sukhanova <katrinsuh@yandex.ru>
2019-04-20 12:01:07 +0300

    grid is working

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import './index.css'
 
 import withStyles from "@material-ui/core/es/styles/withStyles";
 import ArticlePreview from '../ArticlePreview';
@@ -10,18 +11,36 @@ import image from "../../static/icon48.png";
 import Header from "../Header";
 
 const styles = {
-
+    mainArticles: {
+        display: 'grid',
+        gridTemplateColumns: '33% 33% auto',
+        // gridTemplateRows: '300px 300px 300px 100px 100px 100px',
+    },
+    allArticles: {
+        display: 'grid',
+    },
+    newArticle: {
+        gridColumn: 1 / 3,
+        // gridColumnEnd: 3,
+        gridRow: 1 / 3,
+        // gridRowEnd: 4,
+    }
 };
-
 
 class Home extends React.Component {
     render() {
-        let arrayAryicles = [];
+        const { classes } = this.props;
 
         return (
             <div>
                 <h2>Home page</h2>
-                {[1, 2, 3,4].map((item, idx) => <ArticlePreview />)}
+                <div className='wrapper'>
+                    <div className='a'><ArticlePreview/></div>
+                    {[1, 2, 3, 4, 5, 6, 7].map((item, idx) => <div><ArticlePreview/></div>)}
+                </div>
+                {/*<div className={classes.allArticles}>*/}
+                    {/*{[1, 2, 3].map((item, idx) => <ArticlePreview />)}*/}
+                {/*</div>*/}
             </div>
         );
     }
