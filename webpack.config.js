48eb598de0e3f3bd48fbcaa4f4f39824ab6088b8
EkaterinaSukhanova <katrinsuh@yandex.ru>
2019-04-24 15:13:30 +0300

    Add files via upload
    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'src', 'index.js'),
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            components: path.resolve(__dirname, 'src', 'components'),
+            containers: path.resolve(__dirname, 'src', 'containers')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader']
+                })
+
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg|JPG)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true, // webpack@1.x
+                            disable: true, // webpack@2.x and newer
+                        },
+                    },
+                ],
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new ExtractTextPlugin({ filename: 'style.css' }),
+        new HtmlPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+            filename: 'index.html'
+        })
+    ]
+}
